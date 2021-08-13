6f65e66f700533076896eaefb7379a89024849c9
somebeaver <admin@somebeaver.ca>
2021-08-12 22:46:07 -0400

    Animations

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -7,14 +7,15 @@ import { i18n } from '../../i18n/'
 import styles from './Sidebar.module.scss'
 
 /**
- * The Sidebar component.
+ * The Sidebar component. Defaults to 'expanded' mode, which is also the home
+ * page of the app.
  */
 export default function Sidebar({ setModeTo, pages }) {
+  // `window` is not defined when building server side, some code should not run
+  const isSSR = typeof window === 'undefined'
   const modes = ['expanded', 'reading']
   const [mode, setMode] = useState('expanded')
   const [ref, bounds] = useMeasure()
-
-  console.log(bounds)
 
   // useEffect(() => {
   //   if (mode === 'expanded') {
@@ -23,14 +24,6 @@ export default function Sidebar({ setModeTo, pages }) {
   //     setViewMode('reading')
   //   }
   // }, [mode])
-
-  const springEasing = function quadInOut(t) {
-    return ((t *= 2) <= 1 ? t * t : --t * (2 - t) + 1) / 2
-  }
-
-  const percentToPx = (percent) => {
-
-  }
 
   /**
    * Cycle through all possible modes that the sidebar supports.
@@ -49,13 +42,55 @@ export default function Sidebar({ setModeTo, pages }) {
     }
   }
 
-  // const sidebarSpring = useSpring({
-  //   width: mode === 'expanded' ? '100%' : '10%',
-  //   config: {
-  //     duration: 400,
-  //     easing: springEasing
-  //   }}
-  // )
+  /**
+   * Calcualates the position of the inner div, which uses position:fixed to
+   * take up the full screen when in "expanded" mode. When it needs to shrink
+   * back into the sidebar, this function can be used to calculate the position
+   * it needs to be in.
+   *
+   * @param {string} side - `top`', `left`, `right`, `bottom`
+   * @returns {string} - Returns a string like '100px'.
+   */
+  const positionInSidebar = (side) => {
+    if (isSSR) return '0px'
+
+    let px
+
+    switch (side) {
+      case 'top':
+        px = bounds.top
+        break
+      case 'left':
+        px = bounds.left
+        break
+      case 'right':
+        px = window.innerWidth - bounds.right 
+        break
+      case 'bottom':
+        px = window.innerHeight - bounds.bottom 
+        break
+    }
+
+    return `${Math.round(px)}px`
+  }
+
+  function quadInOut(t) {
+    return ((t *= 2) <= 1 ? t * t : --t * (2 - t) + 1) / 2
+  }
+
+  /**
+   * Spring for the .inner element.
+   */
+  const innerSpring = useSpring({
+    top: mode === 'reading' ? positionInSidebar('top') : '25px',
+    left: mode === 'reading' ? positionInSidebar('left') : '25px',
+    right: mode === 'reading' ? positionInSidebar('right') : '25px',
+    bottom: mode === 'reading' ? positionInSidebar('bottom') : '25px',
+    config: {
+      duration: 0,
+      //easing: quadInOut
+    }}
+  )
 
   // const expansionContentSpring = useSpring({
   //   opacity: mode === 'expanded' ? 1 : 0,
@@ -63,13 +98,15 @@ export default function Sidebar({ setModeTo, pages }) {
   //   delay: mode === 'expanded' ? 200 : 0,
   //   config: {
   //     duration: 150,
-  //     easing: springEasing
+  //     easing: quadInOut
   //   }}
   // )
 
   return (
-    <animated.div className={styles.Sidebar} data-mode={mode} ref={ref}>
-      <div className={styles.inner}>
+    <div className={styles.Sidebar} data-mode={mode} ref={ref}>
+      <animated.div 
+        className={`${styles.inner}`}
+        style={innerSpring}>
 
         <div className={styles.controlBar}>
           <header className={styles.header}>
@@ -99,7 +136,7 @@ export default function Sidebar({ setModeTo, pages }) {
           <MegaMenu pages={pages} />
         </animated.div>
 
-      </div>
-    </animated.div>
+      </animated.div>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import { i18n } from '../i18n/'
 
 export default function Home() {
   return (
-    <LayoutDoc
-      seoTitle={i18n('home.seo.title')}
-      seoDesc={i18n('home.seo.desc')}
+    <div
+      // seoTitle={i18n('home.seo.title')}
+      // seoDesc={i18n('home.seo.desc')}
     >
-    </LayoutDoc>
+    </div>
   )
 }
 
