437fe9d8534a42125c2174f15ae05c425f5389e9
somebeaver <admin@somebeaver.ca>
2021-08-14 19:18:37 -0400

    Fixed paths

--- pages/developer/plugins/waveform-plugins.js
+++ pages/developer/plugins/waveform-plugins.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import Icon from '../../components/Icon/Icon'
-import Button from '../../components/Button/Button'
+import Icon from '../../../components/Icon/Icon'
+import Button from '../../../components/Button/Button'
 
 export default function Page() {
   return (
