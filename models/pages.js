93d798646f2ba4c045330e53438e6c5c1fd4e340
somebeaver <admin@somebeaver.ca>
2021-08-14 20:31:32 -0400

    WIP

--- models/pages.js
+++ models/pages.js
@@ -17,11 +17,11 @@ export async function getPages() {
   // Only get the pages that are meant to be part of the Help app.
   // The order here will be the order shown in the frontend.
   let sections = await Promise.all([
-    doGlob(`${PATH_PREFIX}cardinal-server/**/*.js`),
-    doGlob(`${PATH_PREFIX}cardinal-music/**/*.js`),
-    doGlob(`${PATH_PREFIX}general/**/*.js`),
-    doGlob(`${PATH_PREFIX}privacy/**/*.js`),
-    doGlob(`${PATH_PREFIX}developer/**/*.js`),
+    doGlob(`pages/cardinal-server/**/*.js`),
+    doGlob(`pages/cardinal-music/**/*.js`),
+    doGlob(`pages/general/**/*.js`),
+    doGlob(`pages/privacy/**/*.js`),
+    doGlob(`pages/developer/**/*.js`),
   ])
 
   // Convert all pages in all section to PageObject instances
