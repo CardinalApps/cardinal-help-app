a4cf90c7af79c407122eea2f7d70978f24c21990
somebeaver <admin@somebeaver.ca>
2021-09-06 20:16:53 -0400

    Text

--- pages/cardinal-server/Docker.js
+++ pages/cardinal-server/Docker.js
@@ -1,21 +1,29 @@
 import Link from 'next/link'
 import Icon from '../../components/Icon/Icon'
 import Button from '../../components/Button/Button'
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs'
 
 export default function Page() {
   return (
-    <>
+    <div className="document">
       <header>
         <div className="cols">
           <div className="align-content-bottom">
             <Icon type="image" icon="/icons/logo-server.svg" size="l" />
           </div>
+          <div className="align-content-right align-center">
+            <div className="meta">
+             
+            </div>
+          </div>
         </div>
+        <Breadcrumbs path={['Cardinal Server', 'Docker']} />
       </header>
 
       <div className="text">
-        <h1>Default Page</h1>
+        <h1>Cardinal Server for Docker</h1>
+        <p className="coming-soon">The Docker release is currently in development. Check back soon.</p>
       </div>
-    </>
+    </div>
   )
 }
--- pages/cardinal-server/monitoring-connected-devices.js
+++ pages/cardinal-server/monitoring-connected-devices.js
@@ -1,21 +1,36 @@
 import Link from 'next/link'
 import Icon from '../../components/Icon/Icon'
 import Button from '../../components/Button/Button'
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs'
 
 export default function Page() {
   return (
-    <>
+    <div className="document">
       <header>
         <div className="cols">
           <div className="align-content-bottom">
             <Icon type="image" icon="/icons/logo-server.svg" size="l" />
           </div>
+          <div className="align-content-right align-center">
+            <div className="meta">
+             
+            </div>
+          </div>
         </div>
+        <Breadcrumbs path={['Cardinal Server', 'Monitoring Connected Devices']} />
       </header>
 
       <div className="text">
-        <h1>Default Page</h1>
+        <h1>Monitoring Connected Devices</h1>
+        <p>Cardial Server comes with the ability to monitor the devices that are connected to it.</p>
+
+        <h2>Monitoring with the Desktop App</h2>
+        <p>The Cardinal Server desktop app for macOS, Windows, and Linux provides you with an interface for seeing which apps are connected.</p>
+        <img src="/screenshots/server-devices.png" className="med center" alt="Screenshot of the Cardinal Server UI showing connected devices" title="Connected devices" />
+        
+        <h3>Connected Cardinal Music Apps</h3>
+        <p>For connected Cardinal Music client apps, you'll be able to see the currently playing song, it's basic metadata, and the playback method. You'll also be able to pause and resume it, as well as move back and forth through the currently playing queue.</p>
       </div>
-    </>
+    </div>
   )
 }
