76f6509915ee950c6b9e271c67ca334778f2d0cc
somebeaver <admin@somebeaver.ca>
2021-10-01 21:11:50 -0400

    Disable

--- components/HelpApp/HelpApp.js
+++ components/HelpApp/HelpApp.js
@@ -1,18 +1,18 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchPages } from '../../store/slices/pagesSlice'
+// import { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { fetchPages } from '../../store/slices/pagesSlice'
 import styles from './HelpApp.module.scss'
 
 export default function HelpApp({ children }) {
-  const dispatch = useDispatch()
-  const pages = useSelector((store) => store.pages)
+  // const dispatch = useDispatch()
+  // const pages = useSelector((store) => store.pages)
 
-  useEffect(() => {
-    if (!pages.length) {
-      console.log('Hydrating Redux')
-      dispatch(fetchPages())
-    }
-  })
+  // useEffect(() => {
+  //   if (!pages.length) {
+  //     console.log('Hydrating Redux')
+  //     dispatch(fetchPages())
+  //   }
+  // })
 
   return (
     <div id="HelpApp" className={styles.HelpApp}>
